d3d32be12a3b3cbcb19506efbface23e72a65ed7
alyssatiarra <tiarraalyssa@gmail.com>
2024-11-10 13:06:06 +0700

    Login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,7 +32,7 @@ class AuthController extends Controller
             return response()->json([
                 'status' => true,
                 'message' => 'Login Berhasil',
-                'redirect' => url('/')
+                'redirect' => url('/dashboard')
             ]);
             // }
             // return response()->json([
