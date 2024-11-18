4070aaa39e507c53aeaa13a9a2fe76d6a7ee3f03
alyssatiarra <tiarraalyssa@gmail.com>
2024-11-18 09:16:09 +0700

    detail

--- app/Http/Controllers/KegiatanController.php
+++ app/Http/Controllers/KegiatanController.php
@@ -43,12 +43,12 @@ class KegiatanController extends Controller
             return DataTables::of($data)
                 ->addIndexColumn()  // menambahkan kolom index / no urut (default name kolom: DT_RowIndex)  
                 ->addColumn('aksi', function ($kegiatan) {  // menambahkan kolom aksi  
-                    $btn  = '<button onclick="modalAction(\'' . url('/kegiatan/' . $kegiatan['kegiatanId'] .
-                        '/show_ajax') . '\')" class="btn btn-info btn-sm">Detail</button> ';
-                    $btn .= '<button onclick="modalAction(\'' . url('/kegiatan/' . $kegiatan['kegiatanId'] .
-                        '/edit_ajax') . '\')" class="btn btn-warning btn-sm">Edit</button> ';
-                    $btn .= '<button onclick="modalAction(\'' . url('/kegiatan/' . $kegiatan['kegiatanId'] .
-                        '/delete_ajax') . '\')"  class="btn btn-danger btn-sm">Hapus</button> ';
+                    $btn  = '<a href="' . url('/kegiatan/' . $kegiatan['kegiatanId'] .
+                        '/detail') . '" class="btn btn-info btn-sm">Detail</a> ';
+                    // $btn .= '<button onclick="modalAction(\'' . url('/kegiatan/' . $kegiatan['kegiatanId'] .
+                    //     '/edit_ajax') . '\')" class="btn btn-warning btn-sm">Edit</button> ';
+                    // $btn .= '<button onclick="modalAction(\'' . url('/kegiatan/' . $kegiatan['kegiatanId'] .
+                    //     '/delete_ajax') . '\')"  class="btn btn-danger btn-sm">Hapus</button> ';
 
                     return $btn;
                 })
@@ -64,5 +64,50 @@ class KegiatanController extends Controller
         ]);
         $dat = $response->json('data');
         return view('kegiatan.show_ajax', ['kegiatan' => $dat]);
+    }
+
+    public function detailKegiatan(string $id)
+    {
+        $response = Http::withAuthToken()->get("{$this->apiUrl}/api/kegiatan", [
+            'uid' => $id
+        ]);
+        $breadcrumb = (object) [
+            'title' => 'Detail Kegiatan',
+            'list' => ['Kegiatan', 'Detail Kegiatan']
+        ];
+
+        if ($response->successful()) {
+            $data = $response->json('data');
+
+            return view('kegiatan.detail', [
+                'breadcrumb' => $breadcrumb,
+                'activeMenu' => 'apalah',
+                'data' => $data
+            ]);
+        }
+    }
+
+    public function detailUser(Request $request)
+    {
+        $response = Http::withAuthToken()->get("{$this->apiUrl}/api/kegiatan", [
+            'uid' => $request->uid
+        ]);
+
+        if ($response->successful()) {
+            $data = $response->json('data.user');
+            return DataTables::of($data)
+                ->addIndexColumn()  // menambahkan kolom index / no urut (default name kolom: DT_RowIndex)  
+                ->addColumn('aksi', function ($user) {  // menambahkan kolom aksi  
+                    $btn  = '<button onclick="modalAction(\'' . url('/kegiatan/' . $user['userId'] .
+                        '/show_ajax') . '\')" class="btn btn-info btn-sm"> Edit </button> ';
+                    $btn  .= '<button onclick="modalAction(\'' . url('/kegiatan/' . $user['userId'] .
+                        '/show_ajax') . '\')" class="btn btn-warning btn-sm"> Hapus </button> ';
+                    $btn  .= '<button onclick="modalAction(\'' . url('/kegiatan/' . $user['userId'] .
+                        '/show_ajax') . '\')" class="btn btn-danger btn-sm"> Hapus </button> ';
+                    return $btn;
+                })
+                ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html  
+                ->make(true);
+        }
     }
 }
