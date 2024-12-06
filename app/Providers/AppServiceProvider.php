952e004430d44ea59ac563d6baaace63cfa33bc7
lleans <ahmadunnail@gmail.com>
2024-12-06 15:06:13 +0700

    🚀 fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // Define a custom macro with token interceptor
+        if(env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
+
         Http::macro('withAuthToken', function ()  {
             // Retrieve the token from cache
             $token = Cache::get('api_jwt_token');
