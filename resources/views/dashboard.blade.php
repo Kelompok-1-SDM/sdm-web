424ee049a901ec28d09469ffe475f7e91eb52d50
lleans <ahmadunnail@gmail.com>
2024-12-15 00:10:02 +0700

    🛂 fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,21 +21,23 @@
                     </div>
                 </div>
                 <!-- ./col -->
-                <div class="col-lg-4 col-8">
-                    <!-- small box -->
-                    <div class="small-box bg-success">
-                        <div class="inner">
-                            <h3>{{ $data['jumlahManajemen'] }}</h3>
+                @if (session('role') == 'admin')
+                    <div class="col-lg-4 col-8">
+                        <!-- small box -->
+                        <div class="small-box bg-success">
+                            <div class="inner">
+                                <h3>{{ $data['jumlahManajemen'] }}</h3>
 
-                            <p>Jumlah Manajemen</p>
+                                <p>Jumlah Manajemen</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-stats-bars"></i>
+                            </div>
+                            <a href="{{ url('/manajemen') }}" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
                         </div>
-                        <div class="icon">
-                            <i class="ion ion-stats-bars"></i>
-                        </div>
-                        <a href="{{ url('/manajemen') }}" class="small-box-footer">More info <i
-                                class="fas fa-arrow-circle-right"></i></a>
                     </div>
-                </div>
+                @endif
                 <!-- ./col -->
                 <div class="col-lg-4 col-8">
                     <!-- small box -->
