d290f30904a8ec9b9914ab6ba2567eade5466301
lleans <ahmadunnail@gmail.com>
2024-12-19 12:33:19 +0700

    🐛 ngebug

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -266,25 +266,8 @@
 @endsection
 
 @push('js')
-    <!-- jQuery UI 1.11.4 -->
-    <script src="{{ asset('adminlte/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
-    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-    <script>
-        $.widget.bridge('uibutton', $.ui.button)
-    </script>
-    <!-- Bootstrap 4 -->
-    <script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <!-- ChartJS -->
     <script src="{{ asset('adminlte/plugins/chart.js/Chart.min.js') }}"></script>
-    <!-- overlayScrollbars -->
-    <script src="{{ asset('adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
-    {{-- <!-- AdminLTE App --> --}}
-    <script src="{{ asset('adminlte/dist/js/adminlte.js') }}"></script>
-    {{-- <!-- AdminLTE dashboard demo (This is only for demo purposes) --> --}}
-    <script src="{{ asset('adminlte/dist/js/pages/dashboard.js') }}"></script>
-
-    {{-- CDN SweetAlert2 --}}
-    {{-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> --}}
 
     <script>
         $(document).ready(function() {
